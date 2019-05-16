f2424785c37c5af9947819ff0060d825d612bf74
Hassan El-seoudy <31868364+Hassan-Elseoudy@users.noreply.github.com>
2019-05-17 00:01:17 +0200

    Adding projects.

---
+++ Final projects/Digital_Proj_1.m
@@ -0,0 +1,56 @@
+% Project 1
+pkg load communications
+
+%1- Simulation parameters
+numberOfBits = 1e6;
+SNR = 0:2:31;
+m = 10;
+samplingInstant = 10;
+BER  = [];
+BER_ = [];
+
+%2- Generate random binary data vector
+binary_data = randi([0,1] , 1 , 1e6);
+
+%3- Represent each bit with proper waveform
+result = zeros(1, 10e6);
+k = 0;
+for i = 1:10e6
+    if (mod((i - 1), 10) == 0)
+        k = k + 1;
+    end
+    result(i) = binary_data(k);
+end
+
+for i = SNR 
+    %4- Apply noise to samples
+    Rx = awgn(result,i,'measured');
+    size(Rx)
+    %5- Apply convolution process in the receiver
+    %A - Convolution
+    receiver = conv(Rx,result,"same");
+    size(Rx)
+    receiver_ed = zeros(1,1e6);
+    for i = 1:(999999)
+        receiver_ed(i) = receiver((i - 1) * 10 + 5);
+    end
+        receiver_ed(1e6) = receiver(10e6);
+    %B - Correlator
+    correlator = zeros(1,1e6);
+    i = 1;
+    for j = 1:1e6
+        x = Rx(i : i + 9);
+        y = result(i : i + 9);
+        correlator(j) = sum(x.*y);
+        i = i + 10;
+    end
+    %7- Decide whether the Rx_sequence is  1  or  0’
+    out = (receiver_ed > 0);
+    out_ = (correlator > 0);
+    %8- Compare the original bits with the detected bits.
+    [err,ratio] = biterr(binary_data,out);
+    [err_,ratio_] = biterr(binary_data,out_);
+    %9- Save the probability of error of each SNR in matrix , BER
+    BER = [BER ratio];
+    BER_ = [BER_ ratio];
+end
---
+++ Final projects/Digital_Proj_2.m
@@ -0,0 +1,76 @@
+pkg load communications
+
+numberOfBits = 1e6;
+
+SNR = 10.^( (0:2:31) ./10);
+BER = [];
+
+%Generate random binary data vector
+binary_data = randi([0 1] , 1 , numberOfBits);
+
+OOK = binary_data;
+PRK = (2 * binary_data) - 1;
+
+for i = numberOfBits
+    FSK(i) = (binary_data(i) == 0) * 1 + (binary_data(i) == 1) * (0 + 1i);
+end
+
+for i = 1 : 16
+	%Power
+	pow_ = mean(binary_data.^2);
+	%Noise
+	noise = (sqrt(pow_./(2 * SNR(i)) * (randn(size(binary_data)) +1j * randn(size(binary_data)))));
+	%Add some noise to generated bits
+ 	Rx = noise + binary_data;
+ 	%Decide whether the Rx_sequence is ‘1’ or ‘0’
+ 	result = (Rx >= 0.5);
+	%Compare the original bits with the detected bits.
+    	[err,ratio] = biterr(binary_data,result);
+	%Save the probability of error of each SNR in matrix , BER
+	BER = [BER ratio];
+end
+
+semilogy(SNR,BER);                                                    
+grid on;
+
+BER = [];
+for i = 1 : 16
+	%Power
+	pow_ = mean(PRK.^2);
+	%Noise
+	noise = sqrt(pow_./(2 * SNR(i)) * (randn(size(PRK)) + 1j * randn(size(PRK))));
+	%Add some noise to generated bits
+ 	Rx = noise + PRK;
+ 	%Decide whether the Rx_sequence is ‘1’ or ‘0’
+ 	result = (Rx >= 0.5);
+	%Compare the original bits with the detected bits.
+    	[err,ratio] = biterr(binary_data,result);
+	%Save the probability of error of each SNR in matrix , BER
+	BER = [BER err_ratio];
+end
+
+semilogy(SNR,BER);                                                    
+grid on;
+
+BER = [];
+
+for i = 1 : 16
+    %Power
+	pow_ = mean(abs(FSK).^2);
+	%Noise
+	noise = sqrt(pt2./(2*SNR(i))*(randn(size(FSK))+1j*randn(size(FSK))));
+	%Add some noise to generated bits
+ 	Rx = noise + FSK;
+ 	%Decide whether the Rx_sequence is ‘1’ or ‘0’
+ 	A_ = gt(imag(Rx2),real(Rx2));
+	%Compare the original bits with the detected bits.
+    	B_ = xor(A_,binary_data)
+	%Save the probability of error of each SNR in matrix , BER
+	C_(i) = sum(B_,2);
+end
+BER_= C_ ./ 1e6;
+hold on;
+semilogy((0 : 2 : 31),BER_);
+QAM = 0.25 * (2 * erfc(sqrt(2 * SNR/5)) + erfc(3 * sqrt(2 * SNR/5)) + erfc(5 * sqrt(2 * SNR/5)))
+
+semilogy((0 : 2 : 31),QAM)
